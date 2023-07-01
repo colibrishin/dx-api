f0594412922bc390283c07fbab84da997ac56a1e
Colibri Shin <colibrishin@gmail.com>
2023-07-02 00:43:44 +0900

    wrapping x,y with vector2 in character

--- Fortress/Client/character.h
+++ Fortress/Client/character.h
@@ -10,12 +10,14 @@ namespace Object
 		CANNON = 0,
 	};
 
-	class Character : ObjectInternal::_baseObject
+	class character final : public ObjectInternal::_baseObject
 	{
 	public:
-		Character(const float x, const float y, const int hp, const int mp, const CharacterType type)
-		: _baseObject(x, y), m_hp(hp), m_mp(mp), m_type(type) {}
-		
+		character() : _baseObject({0, 0}), m_hp(0), m_mp(0), m_type(CharacterType::CANNON), m_velocity(0, 0) {};
+		character(const Math::Vector2 position, const Math::Vector2 velocity, const int hp, const int mp, const CharacterType type)
+		: _baseObject(position), m_hp(hp), m_mp(mp), m_velocity(velocity), m_type(type) {}
+
+		Math::Vector2 m_velocity;
 	private:
 		int m_hp;
 		int m_mp;
