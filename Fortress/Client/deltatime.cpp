0b19c40264087200b9af308b81f7c59ef790d992
Colibri Shin <colibrishin@gmail.com>
2023-07-07 13:58:19 +0900

    fix deltatime counter flickering

--- Fortress/Client/deltatime.cpp
+++ Fortress/Client/deltatime.cpp
@@ -22,19 +22,12 @@ namespace Fortress
 
 	void DeltaTime::render()
 	{
-		static float interval = 0.0f;
+		wchar_t szFloat[50]{};
+		float fps = 1.0f / m_deltaTime;
 
-		interval += m_deltaTime;
-		if (interval >= 0.01f)
-		{
-			wchar_t szFloat[50]{};
-			float fps = 1.0f / m_deltaTime;
+		swprintf(szFloat, 50, L"DeltaTime: %5f", fps);
+		int strlen = wcsnlen_s(szFloat, 50);
 
-			swprintf(szFloat, 50, L"DeltaTime: %5f", fps);
-			int strlen = wcsnlen_s(szFloat, 50);
-
-			TextOut(WinAPIHandles::get_buffer_dc(), 10, 10, szFloat, strlen);
-			interval = 0.0f;
-		}
+		TextOut(WinAPIHandles::get_buffer_dc(), 10, 10, szFloat, strlen);
 	}
 }
