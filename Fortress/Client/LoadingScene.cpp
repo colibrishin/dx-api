d7f810a9fdd4359d8e8cc9953a8afec27826ed2c
Colibri Shin <colibrishin@gmail.com>
2023-07-14 14:36:51 +0900

    add loading scene

---
+++ Fortress/Client/LoadingScene.cpp
@@ -0,0 +1,33 @@
+#include "LoadingScene.h"
+
+#include "resourceManager.hpp"
+
+void Fortress::Scene::LoadingScene::initialize()
+{
+	scene::initialize();
+	Resource::ResourceManager::load<ImageWrapper>(
+		L"Loading", "./resources/misc/loading/loading.png");
+	m_imBackground = Resource::ResourceManager::Find<ImageWrapper>(L"Loading");
+}
+
+void Fortress::Scene::LoadingScene::update()
+{
+	scene::update();
+}
+
+void Fortress::Scene::LoadingScene::render()
+{
+	scene::render();
+
+	m_imBackground->render({0, 0}, {1.0f, 0.97f});
+}
+
+void Fortress::Scene::LoadingScene::deactivate()
+{
+	scene::deactivate();
+}
+
+void Fortress::Scene::LoadingScene::activate()
+{
+	scene::activate();
+}
---
+++ Fortress/Client/LoadingScene.h
@@ -0,0 +1,28 @@
+#pragma once
+#pragma once
+#ifndef LOADINGSCENE_H
+#define LOADINGSCENE_H
+
+#include "ImageWrapper.hpp"
+#include "sceneManager.hpp"
+
+namespace Fortress::Scene
+{
+	class LoadingScene final : public Abstract::scene
+	{
+	public:
+		LoadingScene() : scene(L"Loading Scene")
+		{
+		}
+
+		void initialize() override;
+		void update() override;
+		void render() override;
+		void deactivate() override;
+		void activate() override;
+
+		ImageWrapper* m_imBackground;
+	};
+}
+
+#endif
