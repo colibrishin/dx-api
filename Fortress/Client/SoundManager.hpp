f34e92a842f1f762e708834e4723d64377d58490
Colibri Shin <colibrishin@gmail.com>
2023-07-30 00:30:23 +0900

    add sound manager

---
+++ Fortress/Client/SoundManager.hpp
@@ -0,0 +1,48 @@
+﻿#ifndef SOUNDMANAGER_HPP
+#define SOUNDMANAGER_HPP
+#include <mmsystem.h>
+#include <dsound.h>
+#include <dinput.h>
+#include <exception>
+
+#include "winapihandles.hpp"
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "dsound.lib")
+
+
+namespace Fortress
+{
+	namespace Resource
+	{
+		class Sound;
+	}
+
+	class SoundManager
+	{
+	public:
+		static LPDIRECTSOUND8 get_device()
+		{
+			return m_sound_device;
+		}
+
+		inline static bool initialize()
+		{
+			if (FAILED(DirectSoundCreate8(NULL, &m_sound_device, NULL)))
+			{
+				throw std::exception("Failed to create direct sound.");
+			}
+			
+			if (FAILED(m_sound_device->SetCooperativeLevel(WinAPIHandles::get_hwnd(), DISCL_EXCLUSIVE))) // Flag�� ����
+			{
+				throw std::exception("Unable to set cooperative level.");
+			}
+
+			return true;
+		}
+
+	private:
+		inline static LPDIRECTSOUND8 m_sound_device = nullptr;
+	};
+}
+#endif // SOUNDMANAGER_HPP
