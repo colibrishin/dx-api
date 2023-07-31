e627140ec5ce4ecaa37b0f7d7a76732ed6ebbf8c
Colibri Shin <colibrishin@gmail.com>
2023-07-31 15:35:17 +0900

    fix title scene

--- Fortress/Client/titleScene.cpp
+++ Fortress/Client/titleScene.cpp
@@ -18,35 +18,11 @@ namespace Fortress::Scene
 
 		Resource::ResourceManager::load<ImageWrapper>(L"Background", "./resources/images/background.jpg");
 		m_imBackground = Resource::ResourceManager::find<ImageWrapper>(L"Background");
-
-		static thread_local std::mt19937 generator(static_cast<unsigned int>(time(nullptr)));
-		std::uniform_int_distribution h_distribution(0, WinAPIHandles::get_window_width());
-		std::uniform_int_distribution w_distribution(0, WinAPIHandles::get_window_height());
-
-		Math::Vector2 random_pos = {
-			static_cast<float>(w_distribution(generator)),
-			static_cast<float>(h_distribution(generator))
-		};
-
-		m_object = std::make_shared<Object::MissileCharacter>(0, L"Ball", random_pos, Math::left);
-
-		add_game_object(Abstract::LayerType::Character, m_object);
-
-		auto rb = std::dynamic_pointer_cast<Abstract::rigidBody>(get_objects().back().lock());
-
-		rb->m_velocity = {1.0f, 1.0f};
-		rb->set_disabled();
-		get_camera().lock()->set_object(rb);
 	}
 
 	void TitleScene::update()
 	{
 		scene::update();
-
-		if(auto rb = std::dynamic_pointer_cast<Abstract::rigidBody>(get_objects().back().lock()))
-		{
-			ObjectBase::character::block_window_frame(rb);
-		}
 
 		if (Input::getKey(eKeyCode::SPACE))
 		{
@@ -71,6 +47,11 @@ namespace Fortress::Scene
 
 	void TitleScene::render()
 	{
+		if(const auto camera = SceneManager::get_active_scene().lock()->get_camera().lock())
+		{
+			m_imBackground.lock()->render({}, m_imBackground.lock()->get_hitbox(), {1.0f, 1.0f});
+		}
+
 		[this]()
 		{
 			wchar_t notice[100] = {};
@@ -78,9 +59,6 @@ namespace Fortress::Scene
 			const size_t strlen = wcsnlen_s(notice, 100);
 			TextOut(WinAPIHandles::get_buffer_dc(), 300, 300, notice, strlen);
 		}();
-
-		m_imBackground.lock()->render({}, {});
-		get_objects().back().lock()->render();
 
 		scene::render();
 	}
