fd63c18f333959bd8a8c0e424daa0926c1b1f4f1
Colibri Shin <colibrishin@gmail.com>
2023-08-07 04:58:58 +0900

    add remove function in global object manager

--- Fortress/Client/objectManager.hpp
+++ Fortress/Client/objectManager.hpp
@@ -9,6 +9,8 @@ namespace Fortress::ObjectBase
 	public:
 		template <typename T, typename... Args>
 		static std::weak_ptr<T> create_object(Args ... args);
+		static void remove_object(const std::weak_ptr<Abstract::object>& obj);
+
 	private:
 		inline static std::vector<std::shared_ptr<Abstract::object>> m_objects = {};
 	};
@@ -18,6 +20,17 @@ namespace Fortress::ObjectBase
 	{
 		m_objects.push_back(std::make_shared<T>(args...));
 		return std::dynamic_pointer_cast<T>(m_objects.back());
+	}
+
+	inline void ObjectManager::remove_object(const std::weak_ptr<Abstract::object>& obj)
+	{
+		m_objects.erase(
+			std::remove_if(m_objects.begin(), m_objects.end(), [obj]
+			(const std::shared_ptr<Abstract::object>& v_obj)
+			{
+				return v_obj == obj.lock();
+			}),
+			m_objects.end());
 	}
 }
 #endif // OBJECTMANAGER_HPP
