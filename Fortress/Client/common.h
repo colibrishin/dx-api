f03cb340033968c279d16768464b21c4439306b0
Colibri Shin <colibrishin@gmail.com>
2023-08-10 01:03:26 +0900

    wrap erase_if namespace for separation

--- Fortress/Client/common.h
+++ Fortress/Client/common.h
@@ -60,11 +60,14 @@ namespace Fortress
 		BottomRight = 10
 	};
 
-	template< typename ContainerT, typename PredicateT >
-	void erase_if( ContainerT& items, const PredicateT& predicate ) {
-    for( auto it = items.begin(); it != items.end(); ) {
-		if( predicate(*it) ) it = items.erase(it);
-		else ++it;
+	namespace Dev
+	{
+		template< typename ContainerT, typename PredicateT >
+		void erase_if( ContainerT& items, const PredicateT& predicate ) {
+	    for( auto it = items.begin(); it != items.end(); ) {
+			if( predicate(*it) ) it = items.erase(it);
+			else ++it;
+			}
 		}
 	}
 }
