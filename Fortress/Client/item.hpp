b281e195011f84233ec1c970b013b38e697ef2fb
Colibri Shin <colibrishin@gmail.com>
2023-08-10 01:01:54 +0900

    friending item with controller for access

--- Fortress/Client/item.hpp
+++ Fortress/Client/item.hpp
@@ -1,6 +1,6 @@
 #ifndef ITEM_HPP
 #define ITEM_HPP
-#include "character.hpp"
+#include "CharacterController.hpp"
 #include "entity.hpp"
 
 namespace Fortress::Object
@@ -14,23 +14,21 @@ namespace Fortress::Object
 		~item() override = default;
 
 		virtual void initialize() = 0;
-		virtual void update(const std::weak_ptr<ObjectBase::character> owner);
+		virtual void update(const std::weak_ptr<ObjectBase::character>& owner) = 0;
 		virtual bool is_effect_ended();
 		bool is_instant() const;
 		virtual void reset();
 		void set_ended();
-
+	protected:
+		void set_hp(const std::weak_ptr<ObjectBase::character>& owner, float hp) const;
+		void equip_nutshell(const std::weak_ptr<ObjectBase::character>& owner);
+		void unequip_nutshell(const std::weak_ptr<ObjectBase::character>& owner);
+		void fire(const std::weak_ptr<ObjectBase::character>& owner) const;
 	private:
 		bool m_instant;
 		bool m_is_used;
 		bool m_is_ended;
 	};
-
-	// base function will flip the used condition.
-	inline void item::update(const std::weak_ptr<ObjectBase::character> owner)
-	{
-		m_is_used = true;
-	}
 
 	inline bool item::is_effect_ended()
 	{
@@ -51,6 +49,30 @@ namespace Fortress::Object
 	inline void item::set_ended()
 	{
 		m_is_ended = true;
+	}
+
+	inline void item::set_hp(const std::weak_ptr<ObjectBase::character>& owner, const float hp) const
+	{
+		const auto casted = owner.lock()->stateController::downcast_from_this<Controller::CharacterController>();
+		casted->set_hp(hp);
+	}
+
+	inline void item::equip_nutshell(const std::weak_ptr<ObjectBase::character>& owner)
+	{
+		const auto casted = owner.lock()->stateController::downcast_from_this<Controller::CharacterController>();
+		casted->equip_nutshell();
+	}
+
+	inline void item::unequip_nutshell(const std::weak_ptr<ObjectBase::character>& owner)
+	{
+		const auto casted = owner.lock()->stateController::downcast_from_this<Controller::CharacterController>();
+		casted->unequip_nutshell();
+	}
+
+	inline void item::fire(const std::weak_ptr<ObjectBase::character>& owner) const
+	{
+		const auto casted = owner.lock()->stateController::downcast_from_this<Controller::CharacterController>();
+		casted->fire();
 	}
 }
 #endif // ITEM_HPP
