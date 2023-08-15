fec901a292bd3c2e79965af662db480f78100114
Colibri Shin <colibrishin@gmail.com>
2023-08-15 22:48:39 +0900

    resource: update with previous expired pointer patch

--- Fortress/Client/resourceManager.hpp
+++ Fortress/Client/resourceManager.hpp
@@ -46,8 +46,9 @@ namespace Fortress::Resource
 			return resource;
 		}
 
-		m_resources[name] = std::make_shared<T>(name, path);
-		return std::dynamic_pointer_cast<T>(m_resources[name]);
+		const auto created = std::make_shared<T>(name, path);
+		m_resources[name] = created;
+		return created;
 	}
 
 	template <typename T>
